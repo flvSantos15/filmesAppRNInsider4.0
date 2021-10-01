5686aa4c87d17cd94a50b33795d4e0cb85a11015
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-09-30 22:32:42 -0300

    Remoção da chave api por segurança

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 // URL FILMES EM CARTAZ
 // https://api.themoviedb.org/3/
 
-// movie/now_playing?api_key=6821d21cebc774e0bab5d9b0c6ca5bc9&language=pt-BR&page=1
 
-export const key = '6821d21cebc774e0bab5d9b0c6ca5bc9'
+// exportar a chave api do site
+//export const key = ''
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3'
